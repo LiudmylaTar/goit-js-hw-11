571d30a599f0460ce472e22ea16d7d1e61217bcb
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-03-10 13:27:42 +0200

    rev-1

---
+++ src/js/render-functions.js
@@ -0,0 +1,54 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const gallery = document.querySelector('.gallery-list');
+const loader = document.querySelector('.loader-wrapper');
+
+export function renderGallery(images) {
+  const markup = images
+    .map(image => {
+      const limitedTags = image.tags.split(', ').slice(0, 4).join(', ');
+
+      return `<li class="gallery-item">
+      <a class="gallery-link" href="${image.largeImageURL}">
+          <img 
+            class="gallery-image" 
+            src= "${image.webformatURL}"
+            alt= "${limitedTags}"
+          />
+      </a>
+       <div class="image-info">
+          <div class="info-titles">
+          <h4>Likes</h4>
+          <p>${image.likes}</p>
+          </div>
+          <div class="info-titles">
+          <h4>Views</h4>
+          <p>${image.views}</p>
+          </div>
+          <div class="info-titles">
+          <h4>Comments</h4>
+          <p>${image.comments}</p>
+           </div>
+          <div class="info-titles">
+          <h4>Downloads</h4>
+          <p>${image.downloads}</p>                 
+          </div>
+        </div>
+  </li>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.style.display = 'block';
+}
+
+export function hideLoader() {
+  loader.style.display = 'none';
+}
---
+++ src/main.js
@@ -0,0 +1,67 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { fetchImages } from './js/pixabay-api';
+import {
+  renderGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+const searchForm = document.querySelector('.search-form');
+const searchBtn = document.querySelector('.search-btn');
+const inputField = document.querySelector('.form-input');
+
+let gallery = new SimpleLightbox('.gallery-list a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: true,
+  nav: true,
+  animationSlide: true,
+  scrollZoom: false,
+});
+
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const query = inputField.value.trim();
+
+  if (query === '') {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search query!',
+      position: 'topRight',
+    });
+    return;
+  }
+  clearGallery();
+  showLoader();
+
+  fetchImages(query)
+    .then(images => {
+      hideLoader();
+
+      if (images.length === 0) {
+        iziToast.warning({
+          title: 'No results',
+          message: 'No images found, try another query!',
+          position: 'topRight',
+        });
+        return;
+      }
+      renderGallery(images);
+      gallery.refresh();
+      inputField.value = '';
+    })
+    .catch(error => {
+      hideLoader();
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong! Try again later.',
+        position: 'topRight',
+      });
+    });
+});
